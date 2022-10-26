16c4398d8e7fd9857abddb187162d69742de1924
Mickael <mickael.rassicot@gmail.com>
2022-10-26 23:22:48 +0200

    [master] work on smooth slider for data display

--- app.js
+++ app.js
@@ -45,14 +45,14 @@ let app = {
 
   calculateTimeBtwTwoDates: function() {
 
-    let quitDate = new Date('01/06/2022 05:23:00');
+    let quitDate = new Date('10/26/2022 11:29:00');
     let today = new Date();
     let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
     let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     let dateTime = date+' '+time
 
     diffDays = Math.floor((Date.UTC(today.getFullYear(), today.getMonth(), today.getDate()) - Date.UTC(quitDate.getFullYear(), quitDate.getMonth(), quitDate.getDate())) / (1000 * 60 * 60 * 24));
-    diffTime = Math.abs(today - quitDate);
+    diffTime = Math.abs(today - quitDate)
 
     quiterSince = app.dhm(diffTime)
 
@@ -63,7 +63,7 @@ let app = {
   loadDynamicInfos: function() {
 
     // Base value
-    cigarettesByDay = 200 // in 24*60 = 1400min
+    cigarettesByDay = 15 // in 24*60 = 1400min
     priceOneCig = 0.465
 
     // let nonSmokinCigarettes = cigarettesByDay * diffDays
@@ -87,6 +87,20 @@ let app = {
 
     timeBetweenACig = Math.round(24 * 60 / cigarettesByDay)
 
+    hourPourcentage = Math.floor((diffTime / 3600000) * 100)
+    twelveHoursPourcentage = Math.floor((diffTime / 43200000) * 100)
+    // We don't want more than 100%
+    
+    successBadge = '<span class="absolute -top-3 right-0"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="w-12 h-12 text-sky-500"><path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg></span>'
+    
+    if (hourPourcentage >= 100) {
+      hourPourcentage = 100
+      $('#hour').append(successBadge)
+    } if (twelveHoursPourcentage >= 100) {
+      twelveHoursPourcentage = 100
+      $('#twelveHours').html(successBadge)
+    }
+
     $('#unsmokedCigarettesLiveValue').empty().html('unsmokedCigarettesLiveValue: ' + unsmokedCigarettesLiveValue)
     $('#unsmokedCigarettesLiveValueRounded').html('unsmokedCigarettesLiveValueRounded: ' + unsmokedCigarettesLiveValueRounded)
     $('#unsmokedCigarettesLiveValueDecimals').html('unsmokedCigarettesLiveValueDecimals: ' + unsmokedCigarettesDecimals)
@@ -95,6 +109,9 @@ let app = {
     $('#moneySaved').html('' + moneySaved.toLocaleString())
     $('#packOfCigs').html('' + Math.floor(packOfCig).toLocaleString())
     $('#burgers').html(burgers)
+
+    $('#hourPourcentage').css('width', hourPourcentage + '%')
+    $('#twelveHoursPourcentage').css('width', twelveHoursPourcentage + '%')
     
     if (cryptoInfo != null) {
       $('#bitcoin').html(bitcoin.toFixed(4))
@@ -141,22 +158,6 @@ let app = {
   updateGauge: function() {
     
     gauge = $('#gauge0').empty()
-    /* $.fn.gauge = function(opts) {
-
-      this.each(function() {
-        var $this = $(this),
-            data = $this.data();
-    
-        if (data.gauge) {
-          data.gauge.stop();
-          delete data.gauge;
-        }
-        if (opts !== false) {
-          data.gauge = new Gauge(this).setOptions(opts);
-        }
-      });
-      return this;
-    }; */
 
     const opts = {
       lineWidth: 0,
@@ -177,7 +178,7 @@ let app = {
     
   },
 
-  dhm: function(ms){
+  dhm: function (ms) {
     const days = Math.floor(ms / (24*60*60*1000));
     const daysms = ms % (24*60*60*1000);
     const hours = Math.floor(daysms / (60*60*1000));
@@ -194,12 +195,10 @@ let app = {
     ]
   }
 
-};
-
+}
 
 $(app.init)
 
 setInterval(function() {
   $(app.updateWithInterval)
 }, 1000)
-
